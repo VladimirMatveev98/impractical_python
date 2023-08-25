71029e096a327ab3522eed65a5a84d3f1704599e
VladimirMatveev98 <89163729496@list.ru>
2023-08-25 16:22:10 +0300

    palingrams

--- chapter_2/palindromes.py
+++ chapter_2/palindromes.py
@@ -2,10 +2,10 @@
 и выводит их на печать"""
 
 import load_dictionary
-world_list = load_dictionary.load("D:/Python's_projects/STUFF/2of4brif.txt")
+word_list = load_dictionary.load("D:/Python's_projects/STUFF/2of4brif.txt")
 palindromes = []
 
-for word in world_list:
+for word in word_list:
     if len(word) > 1 and word == word [::-1]:
         palindromes.append(word)
 
