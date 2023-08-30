c699acf084869cae392baef199b25cc9f03afb29
VladimirMatveev98 <89163729496@list.ru>
2023-08-30 17:19:08 +0300

    phrase_anagrams_created

---
+++ chapter_3/phrase_anagrams.py
@@ -0,0 +1,95 @@
+import sys
+from collections import Counter
+import load_dictionary
+
+#Получить список слов
+dict_file = load_dictionary.load("D:/Python's_projects/STUFF/2of4brif.txt")
+#Удалить однобуквенные "слова"
+dict_file = load_dictionary.optimize(dict_file)
+#Добавить необходимые предлоги 'a' и 'i'
+dict_file.append('a')
+dict_file.append('i')
+dict_file = sorted(dict_file)
+
+ini_name = input("Введите имя: ")
+
+def find_anagrams(name, word_list):
+    """Прочитать имя и список слов и
+    показать все анаграммы в имени"""
+    name_letter_map = Counter(name)
+    anagrams = []
+    for word in word_list:
+        test = ''
+        word_letter_map = Counter(word.lower())
+        for letter in word:
+            if word_letter_map[letter] <= name_letter_map[letter]:
+                test += letter
+            if Counter(test) == word_letter_map:
+                anagrams.append(word)
+
+    print(*anagrams, sep='\n')
+    print()
+    print(f"Оставшиеся буквы = {name}")
+    print(f"Число оставшихся букв = {len(name)}")
+    print(f"Число остальных анаграмм = {len(anagrams)}")
+
+
+def process_choice(name):
+    while True:
+        msg = """\nВведите ИЛИ нажмите ENTER, чтобы начать сначала,
+либо введите # для выхода. >> """
+
+        choice = input(msg)
+        if choice == '':
+            main()
+        elif choice == '#':
+            sys.exit()
+        else:
+            candidate = ''.join(choice.lower().split())
+            left_over_list = list(name)
+            for letter in candidate:
+                if letter in left_over_list:
+                    left_over_list.remove(letter)
+            if len(name) - len(left_over_list) == len(candidate):
+                break
+            else:
+                print("Не сработает, пробуем ещё один вариант.")
+
+    name = ''.join(left_over_list)
+    return choice, name
+
+
+def main():
+    name = ''.join(ini_name.lower().split())
+    name = name.replace('-', '')
+    limit = len(name)
+    phrase = ''
+    running = True
+
+    while running:
+        temp_phrase = phrase.replace(' ', '')
+        if len(temp_phrase) < limit:
+            print(f"Длина анаграммного словосочетания = {len(temp_phrase)}")
+
+            find_anagrams(name, dict_file)
+            print("Текущее анаграммное словосочетание - ", end = '')
+            print(phrase)
+
+            choice, name = process_choice(name)
+            phrase += choice + ' '
+
+        elif len(temp_phrase) == limit:
+            print('\n***** ГОТОВО! *****\n')
+            print(f'Анаграмма имени = {phrase}.')
+            msg = '\n\nПробуем ещё раз -> ENTER, выход -> n. >>'
+            try_again = input(msg)
+            if try_again.lower() == 'n':
+                running = False
+                sys.exit()
+            else:
+                main()
+
+
+
+if __name__ == '__main__':
+    main()
