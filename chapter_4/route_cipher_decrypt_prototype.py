f003b4cc80aa4e58928187edb61fd4ad2af64d72
VladimirMatveev98 <89163729496@list.ru>
2023-09-23 18:40:01 +0300

    add_debug_maode

--- chapter_4/route_cipher_decrypt_prototype.py
+++ chapter_4/route_cipher_decrypt_prototype.py
@@ -1,10 +1,10 @@
 chiper_text = "16 12 8 4 0 1 5 9 13 17 18 14 10 6 2 3 7 11 15 19"
 
+DEBUG = False
 COLS = 4
 ROWS = 5
 KEY = '-1 2 -3 4'
 
-#Разбиваем текст на список слов
 cipher_list = list(chiper_text.split())
 
 translation_matrix = [None] * COLS
@@ -19,14 +19,16 @@ for k in key_int:
         col_items = cipher_list[start:stop]
     elif k > 0:
         col_items = list((reversed(cipher_list[start:stop])))
+
     translation_matrix[abs(k)-1] = col_items
     start += ROWS
     stop += ROWS
 
-
-print(f"Шифр = {chiper_text}\n")
-print(f"Переводная матрица = {translation_matrix}\n")
-print(f"Длина ключа = {len(key_int)}")
+    if DEBUG:
+        print("Новая итерация цикла!")
+        print(f"Шифр = {chiper_text}")
+        print(f"Переводная матрица = {translation_matrix}")
+        print(f"Длина ключа = {len(key_int)}\n")
 
 for i in range(ROWS):
     for col_items in translation_matrix:
