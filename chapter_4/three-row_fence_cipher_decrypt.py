3adc7f25a603bb222ab0264f0743b5a6f858626e
VladimirMatveev98 <89163729496@list.ru>
2023-10-10 19:06:55 +0300

    Усложнённый самостоятельный проект

---
+++ chapter_4/three-row_fence_cipher_decrypt.py
@@ -0,0 +1,73 @@
+"""Дешифровка текста трёхрядным зигзагообразным шрифтом
+времён гражданской войны США.
+При расшифровке восстанавливаются пробелы."""
+
+import math
+import itertools
+
+#------------------------------------------------------------------------------
+#ВХОДНЫЕ ДАННЫЕ:
+cipher_text = """ДАПЕЧ, КИТХМ, НДЕЕАЙ ЕСЁРУ. ДН:ТИЕ ВВВ,РЕ МЕ*ООЁ ВЕ.РЬ"""
+DEBUG = True
+#КОНЕЦ ВХОДНЫХ ДАННЫХ
+#------------------------------------------------------------------------------
+
+stubs = ['!', '?', '.', ',', '*', ';', ':']
+
+def main():
+    """Выполняет программу для дешифровки двухрядного
+    зигзагообразного шрифта."""
+    message = prep_ciphertext(cipher_text)
+    row1, row2, row3 = split_rails(message)
+    message = decrypt(row1, row2, row3)
+
+    if DEBUG:
+        print(f"\nИсходный шифр: {cipher_text}.\n")
+        print(f"Ряд 1: {row1}. \nРяд 2: {row2}.\nРяд 3: {row3}.\n")
+        print(f"Расшифрованное сообщение: {message}.")
+
+
+def prep_ciphertext(cipher_text):
+    """Удаляет пробелы из исходного сообщения и заменяет
+    символы-заглушки на пробелы"""
+    message = "".join(cipher_text.split())
+
+    for sym in stubs:
+        message = message.replace(sym, ' ')
+
+    return message
+
+
+def split_rails(message):
+    """Разбивает исходное сообщение на три"""
+    start = math.ceil(len(message)/3)
+    stop = start * 2
+    row1 = message[:start]
+    row2 = message[start:stop]
+    row3 = message[stop:]
+    print(row1)
+    print(row2)
+    print(row3)
+    return row1, row2, row3
+
+
+def decrypt(row1, row2, row3):
+    """Строит список, чередуя символы из трёх цепочек,
+    и возвращает текст, собранный из этого списка"""
+    plaintext = []
+    for i in range(len(row1)):
+        plaintext.append(row1[i])
+        plaintext.append(row2[i])
+        try:
+            plaintext.append(row3[i])
+        except IndexError:
+            pass
+
+    plaintext = "".join(plaintext)
+
+    return plaintext
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ chapter_4/three-row_fence_cipher_encrypt.py
@@ -0,0 +1,64 @@
+"""Шифровка текста трёхрядным зигзагообразным шрифтом
+времён гражданской войны США. При шифровке пробелы (не?)будут утеряны.
+
+Цели:
+Три ряда вместо двух +
+Сохранять пробелы +
+Сохранять знаки препинания -"""
+
+import random
+
+#----------------------------------------------------------------------------
+#ВХОДНЫЕ ДАННЫЕ:
+plaintext = """Давай пересечём реку и отдохнём в тени деревьев"""
+DEBUG = True
+#КОНЕЦ ВХОДНЫХ ДАННЫХ
+#----------------------------------------------------------------------------
+
+stubs = ['!', '?', '.', ',', '*', ';', ':']
+
+def main():
+    """Выполняет программу шифрования сообщения
+    двухрядным зигзагообразным шрифтом"""
+    message = prep_plaintext(plaintext)
+    rails = build_rails(message)
+    cipher_text = encrypt(rails)
+
+    if DEBUG:
+        print(f"Изначальный текст: {plaintext}")
+        print(f"Зашифрованный текст: {cipher_text}")
+
+
+def prep_plaintext(plaintext):
+    """Удаляет пробелы, заменяя их символами-заглушками"""
+    message = ""
+    for sym in plaintext:
+        if sym != " ":
+            message += sym.upper()
+        else:
+            message += random.choice(stubs)
+
+    return message
+
+
+def build_rails(message):
+    """Строит символьную цепочку, беря каждый третий символ в сообщении"""
+    first = message[::3]
+    second = message[1::3]
+    third = message[2::3]
+
+    rails = first + second + third
+    return rails
+
+
+def encrypt(rails):
+    """Разбивает шифротекст на куски по 6 букв и
+    собирает в одну строку"""
+    cipher_text = " ".join([rails[i:i+6] for i in range(0,len(rails),6)])
+
+    return cipher_text
+
+
+
+if __name__ == '__main__':
+    main()
