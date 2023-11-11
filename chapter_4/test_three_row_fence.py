803fcd792401f275ac153fb62338fe22c618b876
VladimirMatveev98 <89163729496@list.ru>
2023-11-11 20:28:48 +0300

    more tests

--- chapter_4/test_three_row_fence.py
+++ chapter_4/test_three_row_fence.py
@@ -23,25 +23,57 @@ test_case_3 = ("""*""",
 
 
 class Test_encrypt_decrypt(unittest.TestCase):
+    #-------------------Тесты шиврования-----------------------------
     def test_len_message_47_encrypt(self):
+        """Тестирует шифровку сообщения длиной 47 символов"""
         #Заглушки, используемые для пробелов
         stubs = ['!', '?', '.', ',', '*', ';', ':']
 
         #Распаковка данных для проверки
         test_res_1_1 = encrypt.main(test_case_1[0],False)
-        test_res_1_2 = test_case_1[1]
+        correct_res = test_case_1[1]
 
         for sym in stubs:
             #Заменяем "пробелы" на единый символ
-            test_res_1_1 = test_res_1_1.replace(sym, ' ')
-            test_res_1_2 = test_res_1_2.replace(sym, ' ')
+            test_res_1_1 = test_res_1_1.replace(sym, '*')
+            correct_res = correct_res.replace(sym, '*')
 
-        self.assertEqual(test_res_1_1, test_res_1_2)
+        self.assertEqual(test_res_1_1, correct_res)
 
+
+    def test_len_message_47_decrypt(self):
+        """Тестирует расшифровку сообщения длиной 47 символов"""
+        test_res = decrypt.main(test_case_1[1],False)
+        correct_res = test_case_1[0].upper()
+        self.assertEqual(test_res, correct_res)
+
+    #-----------Тесты отдельных функций------------------------
 
     def test_prepare_encrypt(self):
-        """Тестирует функцию удаления пробелов"""
+        """Тестирует функцию подготовки текста к шифровке"""
+        stubs = ['!', '?', '.', ',', '*', ';', ':']
+        test_res = encrypt.prep_plaintext(test_case_1[0])
+
+        for sym in stubs:
+            #Заменяем "пробелы" на единый символ
+            test_res = test_res.replace(sym, '*')
+
+        correct_res = "Давай*пересечём*реку*и*отдохнём*в*тени*деревьев"
+        correct_res = correct_res.upper()
+
+        self.assertEqual(test_res, correct_res)
+
+
+    def test_build_rails(self):
+        """Тестирует построение трёх рядов 'цепочки' """
         pass
+
+
+    def test_encrypt(self):
+        """Тест непостредственно 'сборки' шифротекста по
+        символам 'цепочки', полученной из прошлой функции."""
+        pass
+
 
 
 if __name__ == "__main__":
